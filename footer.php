fc1177dd0db14e8a888543e6d52f965160900872
everaldomatias <matiaseveraldo@gmail.com>
2016-11-26 20:29:32 -0200

    Remove footer da pagina grid

--- footer.php
+++ footer.php
@@ -12,12 +12,6 @@
 
 	</div><!-- #wrapper -->
 
-	<footer id="footer" role="contentinfo">
-		<div class="container">
-			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'odin' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank">Odin</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></p>
-		</div><!-- .container -->
-	</footer><!-- #footer -->
-
 	<?php
 		$form = get_theme_mod( 'form_contato' );
 		if ( !empty( $form ) ) {
