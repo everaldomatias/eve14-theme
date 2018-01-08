5f323b064fef3f54ed7e129bb158d798d4e52d9d
everaldomatias <matiaseveraldo@gmail.com>
2018-01-08 11:18:59 -0200

    Adicionando versao inicial do style.css com SASS funcionando via Grunt.

--- functions.php
+++ functions.php
@@ -1,13 +1,49 @@
 <?php
 
-function e14_setup_theme() {
-	load_theme_textdomain( 'e14', get_template_directory() . '/languages' );
-	add_theme_support( 'custom-header' );
-	add_theme_support( 'custom-logo' );
-	add_theme_support( 'post-thumbnails' );
+/**
+ * Content width.
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = 768;
 }
 
-add_action( 'after_setup_theme', 'e14_setup_theme' );
+if ( ! function_exists( 'e14_setup_theme' ) ) {
+	function e14_setup_theme() {
+		load_theme_textdomain( 'e14', get_template_directory() . '/languages' );
+		add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'custom-header' );
+		add_theme_support( 'custom-logo' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'title-tag' );
+
+		/* HTML 5 */
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption'
+			)
+		);
+	}
+
+	add_action( 'after_setup_theme', 'e14_setup_theme' );
+}
+
+if ( ! function_exists( 'e14_enqueue_scripts' ) ) {
+	function e14_enqueue_scripts() {
+
+		$e14_theme_url = get_template_directory_uri();
+
+		wp_enqueue_style( 'e14-style', $e14_theme_url . '/assets/css/style.css', array(), null, 'all' );
+		//wp_enqueue_script( 'jquery' );
+		
+	}
+
+	add_action( 'wp_enqueue_scripts', 'e14_enqueue_scripts', 1 );
+}
 
 require 'inc/e14-functions.php';
 require 'inc/e14-hooks.php';
