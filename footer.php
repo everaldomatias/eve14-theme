39dd6841b80a75df21d325bd0d3fff9bf8d0f81a
everaldomatias <matiaseveraldo@gmail.com>
2018-01-14 22:34:08 -0200

    Adicionando div's de fechamento no footer.

--- footer.php
+++ footer.php
@@ -1,2 +1,11 @@
-<?php
-wp_footer();
+		
+			</div><!-- /.col-full -->
+		</div><!-- /.content -->
+	</div><!-- hfeed site -->
+
+	<footer id="footer" role="contentinfo">
+	</footer><!-- #footer -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
