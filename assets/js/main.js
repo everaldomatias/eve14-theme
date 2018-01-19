13190c4049678f759c669cf723a302cfab206530
everaldomatias <matiaseveraldo@gmail.com>
2018-01-19 09:34:49 -0200

    Inicia aplicacao de biblioteca AOS para efeito no scroll.

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+	AOS.init({
+		easing: 'ease-out-back',
+		duration: 1000
+	});
+});
--- templates/parts/content-graffiti.php
+++ templates/parts/content-graffiti.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'col-sm-3 nopadding each-graffiti nopadding' ); ?>>
+<article data-aos="fade-up" id="post-<?php the_ID(); ?>" <?php post_class( 'col-sm-3 nopadding each-graffiti nopadding' ); ?>>
 	<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 		<?php the_post_thumbnail( 'thumbnail' ); ?>
 	</a>
