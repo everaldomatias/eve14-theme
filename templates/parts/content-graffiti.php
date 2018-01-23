2d9ba3b40e654c44abc36ec85fd7d54cda8e4a00
everaldomatias <matiaseveraldo@gmail.com>
2018-01-23 21:19:09 -0200

    Responsivo do loop na Home.

--- templates/parts/content-graffiti.php
+++ templates/parts/content-graffiti.php
@@ -1,4 +1,4 @@
-<article data-aos="fade" id="post-<?php the_ID(); ?>" <?php post_class( 'col-sm-3 nopadding each-graffiti nopadding' ); ?>>
+<article data-aos="fade" id="post-<?php the_ID(); ?>" <?php post_class( 'col-xs-6 col-sm-3 col-md-2 nopadding each-graffiti nopadding' ); ?>>
 	<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 		<span class="desc"><?php the_title(); ?></span>
 		<?php the_post_thumbnail( 'thumbnail' ); ?>
