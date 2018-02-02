487a7f5aad355dec463eee168b21fa53033ad7f4
everaldomatias <matiaseveraldo@gmail.com>
2018-02-02 13:36:13 -0200

    Ajustes nos icones do rodape.

--- footer.php
+++ footer.php
@@ -14,19 +14,16 @@
 			<div class="col-sm-4 infos">
 				<?php if ( $telefone = get_theme_mod( 'telefone' ) ): ?>
 					<span itemprop="telephone" class="telefone">
-						<span class="glyphicon glyphicon-phone"></span>
 						<?php echo $telefone; ?>
 					</span><!-- telefone -->
 				<?php endif; ?>
 				<?php if ( $email = get_theme_mod( 'email' ) ): ?>
 					<span class="email">
-						<span class="glyphicon glyphicon-envelope"></span>
 						<a itemprop="email" href="mailto:<?php echo antispambot( $email ); ?>"><?php echo antispambot( $email ); ?></a>
 					</span><!-- email -->
 				<?php endif; ?>
 				<?php if ( $instagram = get_theme_mod( 'instagram' ) ): ?>
 					<span class="instagram">
-						<span class="glyphicon glyphicon-camera"></span>
 						<a target="_blank" href="<?php echo esc_url( "http://instagram.com/" . $instagram ); ?>"><?php echo "@" . $instagram; ?></a>
 					</span><!-- instagram -->
 				<?php endif; ?>
